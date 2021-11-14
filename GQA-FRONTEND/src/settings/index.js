5bdeb45045da463085ac97d96b991153ce32a751
aehm2k5 <e.schulz@hanse-domizil.de>
2021-11-14 12:46:22 +0100

    i18n fix gqausername

--- GQA-FRONTEND/src/settings/index.js
+++ GQA-FRONTEND/src/settings/index.js
@@ -1,6 +1,6 @@
 // 首页等允许无token的白名单
 export const Allowlist = ['/login']
     // 没有用户名的时候使用这个名字
-export const GqaUsername = 'GQA-Username'
+export const GqaUsername = function() { return this.$t('gqaUserName') }
     // 没有头像配置的时候使用这个头像
 export const GqaLoginAvatar = "gqa128.png"
