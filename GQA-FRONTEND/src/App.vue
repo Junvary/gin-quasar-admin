4e6a271fcb06f56a947ee1b23647d620288b21e1
Junvary <junvary@foxmail.com>
2021-11-16 17:35:17 +0800

    动态改变网站标题

--- GQA-FRONTEND/src/App.vue
+++ GQA-FRONTEND/src/App.vue
@@ -4,8 +4,15 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { gqaFrontendMixin } from 'src/mixins/gqaFrontendMixin'
 
 export default defineComponent({
     name: 'App',
+    mixins: [gqaFrontendMixin],
+    watch: {
+        gqaFrontend() {
+            document.title = this.gqaFrontend.gqaSubTitle
+        },
+    },
 })
 </script>
