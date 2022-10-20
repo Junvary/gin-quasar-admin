af0e4c00ba91945f48774fabf3d95f3439c3ca07
junvary <junvary@foxmail.com>
2022-10-20 17:30:07 +0800

    change achievement dialog effect

--- GQA-FRONTEND/src/plugins/Achievement/AchievementDialog.vue
+++ GQA-FRONTEND/src/plugins/Achievement/AchievementDialog.vue
@@ -1,39 +1,48 @@
 <template>
     <q-dialog v-model="visible" position="bottom" persistent transition-show="rotate" transition-hide="rotate">
-        <q-card style="width: 350px; padding: 5px;box-shadow: 0 0 60px 15px orange;" class="bg-orange">
-            <div style="width: 100%; font-size: 0.8rem;" class="text-center text-bold">
-                新成就达成
-            </div>
-            <q-separator dark="" />
-            <q-item v-ripple class="bg-orange">
-                <q-item-section avatar>
-                    <q-chip class="glossy" color="orange-8" text-color="white" icon="star">
+        <GqaCard startColor="#fcbc25" endColor="#f68057" style="width: 500px; box-shadow: 0 0 50px 10px #fcbc25;">
+            <q-card-section>
+                <span class="row justify-center text-h6 text-bold text-white">
+                    新成就解锁
+                </span>
+                <q-separator spaced dark />
+                <div class="row items-center">
+                    <q-chip class="glossy col-2" color="orange-8" text-color="white" icon="emoji_events">
                         {{category}}
                     </q-chip>
-                </q-item-section>
-                <q-item-section style="font-size: 1rem;">
-                    {{name}}
-                </q-item-section>
-            </q-item>
-        </q-card>
+                    <div class="col text-dark row justify-center">
+                        {{name}}
+                    </div>
+                    <q-chip class="glossy col-2" color="orange-8" text-color="white" icon="star">
+                        80
+                    </q-chip>
+                </div>
+            </q-card-section>
+        </GqaCard>
     </q-dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import GqaCard from 'src/components/GqaCard/index.vue'
+import { onBeforeUnmount, ref } from 'vue';
 
 const visible = ref(false)
 
 const category = ref("")
 const name = ref("")
+const timer = ref(null)
 const show = (detail) => {
     category.value = detail?.category
     name.value = detail?.name
     visible.value = true
-    setTimeout(() => {
+    timer.value = setTimeout(() => {
         visible.value = false
     }, 4000)
 }
+
+onBeforeUnmount(() => {
+    clearTimeout(timer.value)
+})
 
 defineExpose({
     show
