8dd5c15dcb30bff731af065340c747a9f18eb7bb
junvary <junvary@foxmail.com>
2022-12-08 08:44:52 +0800

    I18n Classification

---
+++ GQA-FRONTEND/src/i18n/phrases.js
@@ -0,0 +1,62 @@
+export default {
+    WelcomeTo: {
+        "zh-CN": "欢迎使用",
+        "en-US": "Welcome to",
+        "ru": "Добро пожаловать в"
+    },
+    WelcomeBack: {
+        "zh-CN": "欢迎回来",
+        "en-US": "Welcome back",
+        "ru": "С возвращением"
+    },
+    CopyTo: {
+        "zh-CN": "拷贝到",
+        "en-US": "Copy To",
+        "ru": "Копировать в"
+    },
+    CopyToClipboard: {
+        "zh-CN": "拷贝到剪切板",
+        "en-US": "Copy To Clipboard",
+        "ru": "Копировать в буфер обмена"
+    },
+    StartWith: {
+        "zh-CN": "以 {name} 开头",
+        "en-US": "start with {name}",
+        "ru": "начните с {name}"
+    },
+    WithoutOther: {
+        "zh-CN": "不包含其他",
+        "en-US": "with out other",
+        "ru": "без прочих"
+    },
+    KickOut: {
+        "zh-CN": "踢出",
+        "en-US": "Kick Out",
+        "ru": "Выгнать"
+    },
+    SendTo: {
+        "zh-CN": "发送给",
+        "en-US": "Send To",
+        "ru": "Отправить кому"
+    },
+    SelectSingle: {
+        "zh-CN": "单选",
+        "en-US": "Select Single",
+        "ru": "Выбрать одиночный"
+    },
+    SelectMultiple: {
+        "zh-CN": "多选",
+        "en-US": "Select Multiple",
+        "ru": "Выбрать несколько"
+    },
+    RememberMe: {
+        "zh-CN": "记住我",
+        "en-US": "Remember me",
+        "ru": "Запомнить меня"
+    },
+    ViewAll: {
+        "zh-CN": "查看全部",
+        "en-US": "View all",
+        "ru": "Просмотреть все"
+    },
+}
---
+++ GQA-FRONTEND/src/i18n/words.js
@@ -0,0 +1,687 @@
+export default {
+    Register: {
+        "zh-CN": "注册",
+        "en-US": "Register",
+        "ru": "Регистрация"
+    },
+    Page: {
+        "zh-CN": "页面",
+        "en-US": "Page",
+        "ru": "Страница"
+    },
+    SideDrawer: {
+        "zh-CN": "侧边栏",
+        "en-US": "SideDrawer",
+        "ru": "Боковая панель"
+    },
+    Button: {
+        "zh-CN": "按钮",
+        "en-US": "Button",
+        "ru": "Кнопка"
+    },
+    Width: {
+        "zh-CN": "宽度",
+        "en-US": "Width",
+        "ru": "Ширина"
+    },
+    Top: {
+        "zh-CN": "顶部",
+        "en-US": "Top",
+        "ru": "Верх"
+    },
+    Bottom: {
+        "zh-CN": "底部",
+        "en-US": "Bottom",
+        "ru": "Низ"
+    },
+    Welcome: {
+        "zh-CN": "欢迎",
+        "en-US": "Welcome",
+        "ru": "Добро пожаловать"
+    },
+    Version: {
+        "zh-CN": "版本",
+        "en-US": "Version",
+        "ru": "Версия"
+    },
+    Info: {
+        "zh-CN": "信息",
+        "en-US": "Info",
+        "ru": "Информация"
+    },
+    Start: {
+        "zh-CN": "开始",
+        "en-US": "Start",
+        "ru": "Запуск"
+    },
+    Init: {
+        "zh-CN": "初始化",
+        "en-US": "Init",
+        "ru": "Инициализация"
+    },
+    Database: {
+        "zh-CN": "数据库",
+        "en-US": "Database",
+        "ru": "База данных"
+    },
+    Type: {
+        "zh-CN": "类型",
+        "en-US": "Type",
+        "ru": "Тип"
+    },
+    Address: {
+        "zh-CN": "地址",
+        "en-US": "Address",
+        "ru": "Адрес"
+    },
+    Port: {
+        "zh-CN": "端口",
+        "en-US": "Port",
+        "ru": "Порт"
+    },
+    Name: {
+        "zh-CN": "名称",
+        "en-US": "Name",
+        "ru": "Имя"
+    },
+    Username: {
+        "zh-CN": "用户名",
+        "en-US": "Username",
+        "ru": "Имя пользователя"
+    },
+    Password: {
+        "zh-CN": "密码",
+        "en-US": "Password",
+        "ru": "Пароль"
+    },
+    Success: {
+        "zh-CN": "成功",
+        "en-US": "Success",
+        "ru": "Успешно"
+    },
+    Failed: {
+        "zh-CN": "失败",
+        "en-US": "Failed",
+        "ru": "Сбой"
+    },
+    Need: {
+        "zh-CN": "需要",
+        "en-US": "Need",
+        "ru": "Потребность"
+    },
+    Login: {
+        "zh-CN": "登录",
+        "en-US": "Login",
+        "ru": "Войти"
+    },
+    Logout: {
+        "zh-CN": "退出",
+        "en-US": "Logout",
+        "ru": "Выход из системы"
+    },
+    Refresh: {
+        "zh-CN": "刷新",
+        "en-US": "Refresh",
+        "ru": "Обновить"
+    },
+    Authentication: {
+        "zh-CN": "认证",
+        "en-US": "Authentication",
+        "ru": "Аутентификация"
+    },
+    Please: {
+        "zh-CN": "请",
+        "en-US": "Please",
+        "ru": "Пожалуйста"
+    },
+    Relogin: {
+        "zh-CN": "重新登录",
+        "en-US": "Relogin",
+        "ru": "Повторный вход"
+    },
+    Operation: {
+        "zh-CN": "操作",
+        "en-US": "Operation",
+        "ru": "Операция"
+    },
+    Data: {
+        "zh-CN": "数据",
+        "en-US": "Data",
+        "ru": "Данные"
+    },
+    Exception: {
+        "zh-CN": "异常",
+        "en-US": "Exception",
+        "ru": "Исключение"
+    },
+    Timeout: {
+        "zh-CN": "超时",
+        "en-US": "Timeout",
+        "ru": "Тайм-аут"
+    },
+    Request: {
+        "zh-CN": "请求",
+        "en-US": "Request",
+        "ru": "Запрос"
+    },
+    NotFound: {
+        "zh-CN": "未找到",
+        "en-US": "NotFound",
+        "ru": "Не найдено"
+    },
+    Sort: {
+        "zh-CN": "排序",
+        "en-US": "Sort",
+        "ru": "Сортировать"
+    },
+    Config: {
+        "zh-CN": "配置",
+        "en-US": "Config",
+        "ru": "Конфигурация"
+    },
+    Item: {
+        "zh-CN": "项",
+        "en-US": "Item",
+        "ru": "Элемент"
+    },
+    Memo: {
+        "zh-CN": "备注",
+        "en-US": "Memo",
+        "ru": "Памятка"
+    },
+    Default: {
+        "zh-CN": "默认",
+        "en-US": "Default",
+        "ru": "По умолчанию"
+    },
+    Custom: {
+        "zh-CN": "自定义",
+        "en-US": "Custom",
+        "ru": "Пользовательский"
+    },
+    Customize: {
+        "zh-CN": "自定义",
+        "en-US": "Customize",
+        "ru": "Настроить"
+    },
+    Status: {
+        "zh-CN": "状态",
+        "en-US": "Status",
+        "ru": "Статус"
+    },
+    Stable: {
+        "zh-CN": "内置",
+        "en-US": "Stable",
+        "ru": "Стабильный"
+    },
+    Actions: {
+        "zh-CN": "操作",
+        "en-US": "Actions",
+        "ru": "Действия"
+    },
+    Avatar: {
+        "zh-CN": "头像",
+        "en-US": "Avatar",
+        "ru": "Аватар"
+    },
+    Nickname: {
+        "zh-CN": "昵称",
+        "en-US": "Nickname",
+        "ru": "Никнейм"
+    },
+    RealName: {
+        "zh-CN": "真实姓名",
+        "en-US": "RealName",
+        "ru": "Реальное имя"
+    },
+    Gender: {
+        "zh-CN": "性别",
+        "en-US": "Gender",
+        "ru": "Гендер"
+    },
+    Mobile: {
+        "zh-CN": "手机",
+        "en-US": "Mobile",
+        "ru": "Мобильный"
+    },
+    Email: {
+        "zh-CN": "邮箱",
+        "en-US": "E-Mail",
+        "ru": "Электронная почта"
+    },
+    Role: {
+        "zh-CN": "角色",
+        "en-US": "Role",
+        "ru": "Роль"
+    },
+    Dept: {
+        "zh-CN": "部门",
+        "en-US": "Department",
+        "ru": "Отдел"
+    },
+    Reset: {
+        "zh-CN": "重置",
+        "en-US": "Reset",
+        "ru": "Сброс"
+    },
+    Edit: {
+        "zh-CN": "编辑",
+        "en-US": "Edit",
+        "ru": "Редактировать"
+    },
+    Add: {
+        "zh-CN": "新增",
+        "en-US": "Add",
+        "ru": "Добавить"
+    },
+    Delete: {
+        "zh-CN": "删除",
+        "en-US": "Delete",
+        "ru": "Удалить"
+    },
+    Save: {
+        "zh-CN": "保存",
+        "en-US": "Save",
+        "ru": "Сохранить"
+    },
+    Cancel: {
+        "zh-CN": "取消",
+        "en-US": "Cancel",
+        "ru": "Отмена"
+    },
+    Select: {
+        "zh-CN": "选择",
+        "en-US": "Select",
+        "ru": "Выбрать"
+    },
+    All: {
+        "zh-CN": "全部",
+        "en-US": "All",
+        "ru": "Всё"
+    },
+    Search: {
+        "zh-CN": "搜索",
+        "en-US": "Search",
+        "ru": "Поиск"
+    },
+    Confirm: {
+        "zh-CN": "确定",
+        "en-US": "Confirm",
+        "ru": "Подтвердить"
+    },
+    Some: {
+        "zh-CN": "部分",
+        "en-US": "Some",
+        "ru": "Некоторые"
+    },
+    Title: {
+        "zh-CN": "标题",
+        "en-US": "Title",
+        "ru": "Название"
+    },
+    Menu: {
+        "zh-CN": "菜单",
+        "en-US": "Menu",
+        "ru": "Меню"
+    },
+    Api: {
+        "zh-CN": "Api",
+        "en-US": "Api",
+        "ru": "АПИ"
+    },
+    User: {
+        "zh-CN": "用户",
+        "en-US": "User",
+        "ru": "Пользователь"
+    },
+    Manage: {
+        "zh-CN": "管理",
+        "en-US": "Manage",
+        "ru": "Управление"
+    },
+    System: {
+        "zh-CN": "系统",
+        "en-US": "System",
+        "ru": "Система"
+    },
+    Dashboard: {
+        "zh-CN": "仪表盘",
+        "en-US": "Dashboard",
+        "ru": "Панель"
+    },
+    Frontend: {
+        "zh-CN": "前端",
+        "en-US": "Frontend",
+        "ru": "Интерфейс"
+    },
+    Backend: {
+        "zh-CN": "后端",
+        "en-US": "Backend",
+        "ru": "Бэкенд"
+    },
+    Log: {
+        "zh-CN": "日志",
+        "en-US": "Log",
+        "ru": "Журнал"
+    },
+    Parent: {
+        "zh-CN": "父级",
+        "en-US": "Parent",
+        "ru": "Родитель"
+    },
+    Children: {
+        "zh-CN": "子级",
+        "en-US": "Children",
+        "ru": "Ребёнок"
+    },
+    Code: {
+        "zh-CN": "编码",
+        "en-US": "Code",
+        "ru": "Код"
+    },
+    Permission: {
+        "zh-CN": "权限",
+        "en-US": "Permission",
+        "ru": "Разрешение"
+    },
+    Dict: {
+        "zh-CN": "字典",
+        "en-US": "Dict",
+        "ru": "Справочник"
+    },
+    Detail: {
+        "zh-CN": "详情",
+        "en-US": "Detail",
+        "ru": "Детали"
+    },
+    Browser: {
+        "zh-CN": "浏览器",
+        "en-US": "Browser",
+        "ru": "Браузер"
+    },
+    Os: {
+        "zh-CN": "操作系统",
+        "en-US": "Operation System",
+        "ru": "Операционная система"
+    },
+    Platform: {
+        "zh-CN": "平台",
+        "en-US": "Platform",
+        "ru": "Платформа"
+    },
+    Method: {
+        "zh-CN": "方法",
+        "en-US": "Method",
+        "ru": "Метод"
+    },
+    Read: {
+        "zh-CN": "阅读",
+        "en-US": "Read",
+        "ru": "Запись"
+    },
+    Send: {
+        "zh-CN": "发送",
+        "en-US": "Send",
+        "ru": "Отправить"
+    },
+    Unknown: {
+        "zh-CN": "未知",
+        "en-US": "Unknown",
+        "ru": "Неизвестно"
+    },
+    Captcha: {
+        "zh-CN": "验证码",
+        "en-US": "Captcha",
+        "ru": "Captcha"
+    },
+    Error: {
+        "zh-CN": "错误",
+        "en-US": "Error",
+        "ru": "Ошибка"
+    },
+    Gen: {
+        "zh-CN": "生成",
+        "en-US": "Generate",
+        "ru": "Генерировать"
+    },
+    Plugin: {
+        "zh-CN": "插件",
+        "en-US": "Plugin",
+        "ru": "Плагин"
+    },
+    Icon: {
+        "zh-CN": "图标",
+        "en-US": "icon",
+        "ru": "Иконка"
+    },
+    Phone: {
+        "zh-CN": "电话",
+        "en-US": "Phone",
+        "ru": "Телефон"
+    },
+    New: {
+        "zh-CN": "新",
+        "en-US": "New",
+        "ru": "Новый"
+    },
+    Old: {
+        "zh-CN": "旧",
+        "en-US": "Old",
+        "ru": "Старый"
+    },
+    Sent: {
+        "zh-CN": "已发送",
+        "en-US": "Sent",
+        "ru": "Отправить"
+    },
+    Content: {
+        "zh-CN": "内容",
+        "en-US": "Content",
+        "ru": "Контент"
+    },
+    Switch: {
+        "zh-CN": "切换",
+        "en-US": "Switch",
+        "ru": "Переключатель"
+    },
+    Language: {
+        "zh-CN": "语言",
+        "en-US": "Language",
+        "ru": "Язык"
+    },
+    English: {
+        "zh-CN": "英文",
+        "en-US": "English",
+        "ru": "Английский"
+    },
+    Unique: {
+        "zh-CN": "唯一",
+        "en-US": "Unique",
+        "ru": "Уникальный"
+    },
+    Column: {
+        "zh-CN": "列",
+        "en-US": "Column",
+        "ru": "Столбец"
+    },
+    Hidden: {
+        "zh-CN": "隐藏",
+        "en-US": "Hidden",
+        "ru": "Скрыто"
+    },
+    Component: {
+        "zh-CN": "组件",
+        "en-US": "Component",
+        "ru": "Компонент"
+    },
+    Change: {
+        "zh-CN": "修改",
+        "en-US": "Change",
+        "ru": "Изменение"
+    },
+    File: {
+        "zh-CN": "文件",
+        "en-US": "File",
+        "ru": "Файл"
+    },
+    Close: {
+        "zh-CN": "关闭",
+        "en-US": "Close",
+        "ru": "закрыть"
+    },
+    Installed: {
+        "zh-CN": "已安装",
+        "en-US": "Installed",
+        "ru": "Установлено"
+    },
+    Describe: {
+        "zh-CN": "描述",
+        "en-US": "Describe",
+        "ru": "Описать"
+    },
+    Choose: {
+        "zh-CN": "选择",
+        "en-US": "Choose",
+        "ru": "Выбрать"
+    },
+    Dark: {
+        "zh-CN": "黑色",
+        "en-US": "Dark",
+        "ru": "чёрный"
+    },
+    Theme: {
+        "zh-CN": "主题",
+        "en-US": "Theme",
+        "ru": "Тема"
+    },
+    Display: {
+        "zh-CN": "显示",
+        "en-US": "Display",
+        "ru": "Дисплей"
+    },
+    Style: {
+        "zh-CN": "风格",
+        "en-US": "Style",
+        "ru": "Стиль"
+    },
+    Color: {
+        "zh-CN": "颜色",
+        "en-US": "Color",
+        "ru": "Цвет"
+    },
+    Basic: {
+        "zh-CN": "基础",
+        "en-US": "Basic",
+        "ru": "Базовый"
+    },
+    Setting: {
+        "zh-CN": "设置",
+        "en-US": "Setting",
+        "ru": "Настройки"
+    },
+    Notice: {
+        "zh-CN": "消息",
+        "en-US": "Notice",
+        "ru": "Уведомления"
+    },
+    Done: {
+        "zh-CN": "已完成",
+        "en-US": "Done",
+        "ru": "Сделано"
+    },
+    Size: {
+        "zh-CN": "大小",
+        "en-US": "Size",
+        "ru": "Размер"
+    },
+    Progress: {
+        "zh-CN": "进度",
+        "en-US": "Progress",
+        "ru": "Прогресс"
+    },
+    Upload: {
+        "zh-CN": "上传",
+        "en-US": "Upload",
+        "ru": "Загрузить"
+    },
+    Clear: {
+        "zh-CN": "清除",
+        "en-US": "Clear",
+        "ru": "Очистить"
+    },
+    Group: {
+        "zh-CN": "分组",
+        "en-US": "Group",
+        "ru": "Группа"
+    },
+    Path: {
+        "zh-CN": "路径",
+        "en-US": "Path",
+        "ru": "Путь"
+    },
+    Redirect: {
+        "zh-CN": "重定向",
+        "en-US": "Redirect",
+        "ru": "Перенаправление"
+    },
+    Leader: {
+        "zh-CN": "负责人",
+        "en-US": "Leader",
+        "ru": "Лидер"
+    },
+    Loading: {
+        "zh-CN": "加载中",
+        "en-US": "Loading",
+        "ru": "Загрузка"
+    },
+    Copy: {
+        "zh-CN": "拷贝",
+        "en-US": "Copy",
+        "ru": "Копия"
+    },
+    Clipboard: {
+        "zh-CN": "剪切板",
+        "en-US": "Clipboard",
+        "ru": "Буфер обмена"
+    },
+    On: {
+        "zh-CN": "启用",
+        "en-US": "On",
+        "ru": "Влючено"
+    },
+    Off: {
+        "zh-CN": "禁用",
+        "en-US": "Off",
+        "ru": "Выключено"
+    },
+    Yes: {
+        "zh-CN": "是",
+        "en-US": "Yes",
+        "ru": "Да"
+    },
+    No: {
+        "zh-CN": "否",
+        "en-US": "No",
+        "ru": "Нет"
+    },
+    Male: {
+        "zh-CN": "男",
+        "en-US": "Male",
+        "ru": "Мужчина"
+    },
+    Female: {
+        "zh-CN": "女",
+        "en-US": "Female",
+        "ru": "Женщина"
+    },
+    Without: {
+        "zh-CN": "不包含",
+        "en-US": "with out",
+        "ru": "кроме"
+    },
+    Your: {
+        "zh-CN": "你的",
+        "en-US": "Your",
+        "ru": "Ваш"
+    },
+}
